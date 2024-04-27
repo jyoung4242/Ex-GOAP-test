d8072dc3534d7f55b8b05ae0c72d7de3bfc16d56
Justin Young <justin_dean_young@yahoo.com>
2024-04-27 13:20:42 -0400

    build step

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
+import topLevelAwait from "vite-plugin-top-level-await";
+
 export default {
+  plugins: [
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: "default",
+    }),
+  ],
   base: "./",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 };
